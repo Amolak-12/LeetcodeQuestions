cceff69a53f75dd78cbb50502001c6a213cb6329
AmolakKumar <8895778-AmolakKumar@users.noreply.replit.com>
2023-05-17 07:25:47 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Leetcode2/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+  vector<int>maximini{1,2,5,-16,15,27,35,28,33,59,54,-54};
+  int maxi = -99999;
+  int mini = 9999999;
+
+  for(int i=0;i<maximini.size();i++){
+    if(maximini[i] > maxi)
+    {
+      maxi = maximini[i];
+    }
+  }
+  for(int i=0;i<maximini.size();i++){
+     if(maximini[i] < mini)
+    {
+      mini = maximini[i];
+    }
+  }
+  cout << "Maxi number : " << maxi << endl << "Mini number : " << mini << endl;
+}
